b08d197cc544699df7b030b6537e65cf8092e5a5
Quamatic <lman71037@gmail.com>
2023-04-23 22:13:03 -0500

    Fix wrong logic

--- src/hooks/useReduxContext.lua
+++ src/hooks/useReduxContext.lua
@@ -4,7 +4,7 @@ local ReactReduxContext = require(script.Parent.Parent.components.Context)
 local function useReduxContext(): ReactReduxContext.ReactReduxContextValue<any, any>?
 	local contextValue = React.useContext(ReactReduxContext)
 
-	if not _G.__DEV__ and not contextValue then
+	if _G.__DEV__ and not contextValue then
 		error("could not find react-redux context value; please ensure the component is wrapped in a <Provider>")
 	end
 
